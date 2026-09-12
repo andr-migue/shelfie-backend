e1874729b4f983b10d2d818e18c96b2e90d2b732
Miguel Cazorla Zamora <miguelzamora210405@gmail.com>
2026-09-12 11:59:51 -0400

    add async HTTP client and database initialization in lifespan context

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+import httpx
+from app.core.config import settings
+from app.core.db import init_db
 
 @asynccontextmanager
 async def lifespan(app):
+    mongo_client = await init_db()
+    
+    app.state.http_client = httpx.AsyncClient(
+        base_url=settings.open_library_base_url,
+        timeout=settings.http_timeout
+    )
+    
     yield
+    
+    await app.state.http_client.aclose()
+    await mongo_client.close()
 
 app = FastAPI(lifespan=lifespan)
