5971a648b2242bd799d1b75c3cb3fe7955862263
Miguel Cazorla Zamora <miguelzamora210405@gmail.com>
2026-09-12 21:51:42 -0400

    feat: include library router in API router

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.routers import books, catalog
+from app.routers import books, catalog, library
 
 api_router = APIRouter()
 
 api_router.include_router(books.router)
 api_router.include_router(catalog.router)
+api_router.include_router(library.router)
